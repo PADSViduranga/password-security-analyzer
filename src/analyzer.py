d97b1672e1a8b870d10d37d650e7db981bbcc00b
Sachintha Viduranga <padsviduranga@students.nsbm.ac.lk>
2026-08-25 18:37:24 +0530

    Delete src/analyzer.py

--- src/analyzer.py
+++
@@ -1,9 +0,0 @@
-def analyze_password(password):
-    result={
-        "length": len(password),
-        "has_uppercase": any(char.isupper() for char in password),
-        "has_lowercase": any(char.islower() for char in password),
-        "has_digit": any(char.isdigit() for char in password),
-        "has_special": any(not char.isalnum() for char in password)
-        }
-    return result
