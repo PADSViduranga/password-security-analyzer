4fc1a87f17d3d06ba5ca49ebb1c0808b1693dce3
Your Name <padsviduranga@students.nsbm.ac.lk>
2026-08-25 18:30:44 +0530

    resolve merge conflicts

--- src/main.py
+++ src/main.py
@@ -4,14 +4,15 @@ from strength import classify_strength
 from patterns import detect_patterns
 from recommendations import generate_recommendations
 
+
 def main():
-    print("="*45)
+    print("=" * 45)
     print("Welcome to the Password Analyzer!")
-    print("="*45)
+    print("=" * 45)
 
     password = input("Enter a password to analyze: ")
     analysis_result = analyze_password(password)
-    
+
     print("\nPassword Analysis:")
     print(f"Length: {analysis_result['length']}")
     print(f"Contains Uppercase: {analysis_result['has_uppercase']}")
@@ -25,27 +26,30 @@ def main():
     print(f"\nCharacter Pool Size: {pool}")
     print(f"Estimated Entropy: {entropy:.2f} bits")
 
-    strength=classify_strength(entropy)
+    strength = classify_strength(entropy)
     print(f"Password Strength: {strength}")
 
-    Warning=detect_patterns(password)
+    warnings = detect_patterns(password)
 
-    if Warning:
-        print("\nSecurity warnings")
-        for warning in Warning:
-             print(f"⚠ {warning}")
-
+    if warnings:
+        print("\nSecurity Warnings:")
+        for warning in warnings:
+            print(f"⚠ {warning}")
     else:
         print("\nSecurity Warnings:")
         print("✓ No obvious predictable patterns detected")
 
-    reccomendations=generate_recommendations(
+    recommendations = generate_recommendations(
         password,
         analysis_result,
-        Warning,
-        entropy)
+        warnings,
+        entropy
+    )
 
-    for recommendation in reccomendations:
+    print("\nRecommendations:")
+    for recommendation in recommendations:
         print(f"💡 {recommendation}")
+
+
 if __name__ == "__main__":
     main()
